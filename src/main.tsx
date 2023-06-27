4f17aece4989417f24f9ea58ffadd6c4071b57bf
nigel <nigel.nelles@googlemail.com>
2023-06-28 00:58:21 +0200

    install material ui

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { BrowserRouter } from 'react-router-dom';
+import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 
-
-import {firebaseApp} from "./firebase/firebase.ts";
-console.log(firebaseApp)
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Roboto, sans-serif', // Set Roboto as the default font
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App/>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App/>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
